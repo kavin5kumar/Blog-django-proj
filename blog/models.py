1561df4413c5213feebb8a51990e7f94b86432a1
Kavin Kumar <kavin2161@Musique-Kavin.local>
2021-08-04 08:06:41 +0530

    added accounts and templates

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.urls import reverse
+
 
 # Create your models here.
 class Post(models.Model):
@@ -12,3 +13,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView
 
 from .models import Post
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -12,4 +14,17 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post-detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post-new.html"
+    fields = '__all__'
 
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post-edit.html"
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post-delete.html"
+    success_url = reverse_lazy('home')
